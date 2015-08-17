e6d57cb22a9000ad4c289c2b67328bb9905644c5
ramonornela <ramon.ornela@gmail.com>
2015-08-17 11:43:13 -0300

    [UPDATE]
    - Correcao stream de arquivos

--- src/reporters/index.js
+++ src/reporters/index.js
@@ -5,57 +5,57 @@ var _ = require('lodash');
 exports.failReporter = require('./fail');
 
 exports.loadReporter = function (reporter) {
-  // we want the function
-  if (typeof reporter === 'function') return reporter;
+    // we want the function
+    if (typeof reporter === 'function') return reporter;
 
-  // object reporters
-  if (typeof reporter === 'object' && typeof reporter.reporter === 'function') return reporter.reporter;
+    // object reporters
+    if (typeof reporter === 'object' && typeof reporter.reporter === 'function') return reporter.reporter;
 
-  // load jshint built-in reporters
-  if (typeof reporter === 'string') {
-    try {
-      return exports.loadReporter(require('jshint/src/reporters/' + reporter));
-    } catch (err) {}
-  }
+    // load jshint built-in reporters
+    if (typeof reporter === 'string') {
+        try {
+            return exports.loadReporter(require('jshint/src/reporters/' + reporter));
+        } catch (err) {}
+    }
 
-  // load full-path or module reporters
-  if (typeof reporter === 'string') {
-    try {
-      return exports.loadReporter(require(reporter));
-    } catch (err) {}
-  }
+    // load full-path or module reporters
+    if (typeof reporter === 'string') {
+        try {
+            return exports.loadReporter(require(reporter));
+        } catch (err) {}
+    }
 };
 
 exports.reporter = function (reporter, reporterCfg) {
-  reporterCfg = reporterCfg || {};
+    reporterCfg = reporterCfg || {};
 
-  if (reporter === 'fail') {
-    return exports.failReporter(reporterCfg);
-  }
-
-  var rpt = exports.loadReporter(reporter || 'default'),
-      object = {
-          results: [],
-          data: [],
-          options: []
-      }, opt;
-
-  if (typeof rpt !== 'function') {
-    throw new PluginError('gulp-jshint', 'Invalid reporter');
-  }
-
-  // return stream that reports stuff
-  return stream(function (file, cb) {
-    if (file.jshint && !file.jshint.success && !file.jshint.ignored) {
-      // merge the reporter config into this files config
-      opt = _.defaults({}, reporterCfg, file.jshint.opt);
-
-      object.results.push(file.jshint.results);
-      object.data.push(file.jshint.data);
+    if (reporter === 'fail') {
+        return exports.failReporter(reporterCfg);
     }
 
-    cb(null, file);
-  }, function(cb) {
-      rpt(object.results, object.data, opt);
-  });
+    var rpt = exports.loadReporter(reporter || 'default'),
+        object = {
+            results: [],
+            data: []
+        }, opt;
+
+    if (typeof rpt !== 'function') {
+        throw new PluginError('gulp-jshint', 'Invalid reporter');
+    }
+
+    // return stream that reports stuff
+    return stream(function (file, cb) {
+        if (file.jshint && !file.jshint.success && !file.jshint.ignored) {
+            // merge the reporter config into this files config
+            opt = _.defaults({}, reporterCfg, file.jshint.opt);
+
+            object.results = object.results.concat(file.jshint.results);
+            object.data = object.data.concat(file.jshint.data);
+        }
+
+        cb(null, file);
+    }, function(cb) {
+        rpt(object.results, object.data, opt);
+        cb();
+    });
 };
